54dbc57010c028c14f8cb7bf08734c3e44851d78
Daniel Trnka <daniel.trnka@gmail.com>
2025-09-13 22:24:41 +0000

    dts overlay for pwm

--- system/setup.sh
+++ system/setup.sh
@@ -2,6 +2,11 @@
 set -ex
 RES_PATH=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
+dtc -@ -I dts -O dtb car-overlay.dts > /boot/dtbs/car-overlay.dtbo
+
+
+exit 1
+
 if [ "$UID" -eq 0 ]; then
 	echo "Run as non-root"
 	exit 1
