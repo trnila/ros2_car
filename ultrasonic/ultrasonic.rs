c1fe6316426c12be7bae385d4a1a676001061fe4
Daniel Trnka <daniel.trnka@gmail.com>
2025-09-14 18:38:11 +0000

    better error handling

--- ultrasonic/ultrasonic.rs
+++ ultrasonic/ultrasonic.rs
@@ -3,15 +3,32 @@ use gpiocdev::line::EdgeEvent;
 use gpiocdev::line::EdgeKind;
 use gpiocdev::line::Value;
 use gpiocdev::{FoundLine, Request};
+use rclrs::ToLogParams;
+use rclrs::{log_error, log_warn};
 use std::env;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
+use std::time::Instant;
 
 pub struct Ultrasonic {
     trigger_line: FoundLine,
     req: Request,
+}
+
+pub enum MeasurementError {
+    /// ultrasonic beam was not sent
+    NoTrigger,
+    /// ultrasonic beam was not received back
+    NoEcho,
+    /// Unexpected edge received
+    WrongEdge,
+}
+
+enum EdgeError {
+    Timeout,
+    WrongEdge,
 }
 
 impl Default for Ultrasonic {
@@ -34,17 +51,20 @@ impl Default for Ultrasonic {
 }
 
 impl Ultrasonic {
-    fn wait_event(&self, kind: EdgeKind, timeout: Duration) -> Option<EdgeEvent> {
+    fn wait_event(&self, kind: EdgeKind, timeout: Duration) -> Result<EdgeEvent, EdgeError> {
         if self.req.wait_edge_event(timeout).unwrap() {
             let event = self.req.read_edge_event().unwrap();
-            assert!(event.kind == kind);
-            Some(event)
+            if event.kind == kind {
+                Ok(event)
+            } else {
+                Err(EdgeError::WrongEdge)
+            }
         } else {
-            None
+            Err(EdgeError::Timeout)
         }
     }
 
-    fn measure(&self) -> f32 {
+    fn measure(&self) -> Result<f32, MeasurementError> {
         // clear all echo events
         while self.req.has_edge_event().unwrap() {
             self.req.read_edge_event().unwrap();
@@ -60,17 +80,22 @@ impl Ultrasonic {
             .unwrap();
 
         // wait for the start of ultrasound transmission
-        let transmitted = self.wait_event(EdgeKind::Rising, Duration::from_millis(1));
-        if let Some(transmitted) = transmitted {
-            // wait when signals is returned back
-            let received = self.wait_event(EdgeKind::Falling, Duration::from_millis(50));
-            if let Some(received) = received {
-                let time_diff = received.timestamp_ns - transmitted.timestamp_ns;
-                let cm = time_diff as f32 * 1e-9 * 17150f32;
-                return cm;
-            }
-        }
-        f32::NAN
+        let transmitted = self
+            .wait_event(EdgeKind::Rising, Duration::from_millis(1))
+            .map_err(|err| match err {
+                EdgeError::Timeout => MeasurementError::NoTrigger,
+                EdgeError::WrongEdge => MeasurementError::WrongEdge,
+            })?;
+        // wait when signals is returned back
+        let received = self
+            .wait_event(EdgeKind::Falling, Duration::from_millis(50))
+            .map_err(|err| match err {
+                EdgeError::Timeout => MeasurementError::NoEcho,
+                EdgeError::WrongEdge => MeasurementError::WrongEdge,
+            })?;
+        let time_diff = received.timestamp_ns - transmitted.timestamp_ns;
+        let cm = time_diff as f32 * 1e-9 * 17150f32;
+        Ok(cm)
     }
 }
 
@@ -92,14 +117,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let ultrasonic = Ultrasonic::default();
+    let rate = Duration::from_millis(50);
+    let mut next_tick = Instant::now();
     while !shut_down.load(Ordering::Relaxed) && context.ok() {
-        let distance_cm = ultrasonic.measure();
-
-        message.range = distance_cm;
-
-        //log_info!(node.logger(), "Publishing: {}", message.data);
+        let measurement = ultrasonic.measure();
+        message.range = match measurement {
+            Ok(cm) => cm,
+            Err(MeasurementError::NoTrigger) => {
+                log_error!(node.logger(), "No trigger pulse detected");
+                f32::NAN
+            }
+            Err(MeasurementError::NoEcho) => {
+                log_warn!(node.logger(), "No echo pulse received");
+                f32::NAN
+            }
+            Err(MeasurementError::WrongEdge) => {
+                log_error!(node.logger(), "Unexpected signal edge detected");
+                f32::NAN
+            }
+        };
         publisher.publish(&message)?;
-        std::thread::sleep(std::time::Duration::from_millis(500));
+
+        next_tick += rate;
+        let now = Instant::now();
+        if now < next_tick {
+            std::thread::sleep(next_tick - now);
+        } else {
+            next_tick = now;
+            log_warn!(node.logger(), "Ultrasonic measurement missed its deadline");
+        }
     }
 
     Ok(())
